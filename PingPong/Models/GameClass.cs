7757886d23a1540afaefc8f3a0affbd6ed1895f0
object-ions <moshikoatia@gmail.com>
2023-07-10 15:21:12 -0700

    run first test and pass
    Co-Authored-By: Jason Church elijahchurch@gmail.com

--- PingPong/Models/GameClass.cs
+++ PingPong/Models/GameClass.cs
@@ -1,7 +1,24 @@
-namespace ProjectName.Models
+using System.Collections.Generic;
+using System;
+
+namespace PingPong.Models
 {
-    public class ClassName
+    public class GameClass
     {
+        public static Dictionary<int, object> _gameResult = new Dictionary<int, object> {};
         // Business Logic goes here.
+
+        public static Dictionary<int, object> CountUp(int userInput)
+        {
+            _gameResult.Clear();
+
+            for (int i = 1; i <= userInput; i++)
+            {
+                _gameResult.Add(i, i);
+            }
+
+            return _gameResult;
+        }
+
     }
 }
--- PingPong/Program.cs
+++ PingPong/Program.cs
@@ -1,4 +1,4 @@
-namespace ProjectName
+namespace PingPong.Models
 {
     class Program
     {
